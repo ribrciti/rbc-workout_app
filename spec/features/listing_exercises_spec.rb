9d1b130c4ddfc34913363fe530200e8d7cc5d49d
Rick <rbciti@msn.com>
2016-10-16 09:36:25 -0700

    Added listing Exercises

---
+++ spec/features/listing_exercises_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Listing Exercises" do
+  before do
+    @test = User.create!(email: "test@test.com", password: "asdfasdf", first_name: "Wesson", last_name: "Smith")
+    login_as(@test)
+
+    @e1 = @test.exercises.create(duration_in_min: 33, workout: "Weightlifting with dumbells", workout_date: "2016-08-11")
+    @e2 = @test.exercises.create(duration_in_min: 66, workout: "Ride Platonic Stationary Bike", workout_date: "2016-08-12")
+
+  end
+
+  scenario "shows user's workout for last 7 days" do 
+    visit "/"
+    
+    click_link("My Lounge")
+
+    expect(page).to have_content(@e1.duration_in_min)
+    expect(page).to have_content(@e1.workout)
+    expect(page).to have_content(@e1.workout_date)
+
+    expect(page).to have_content(@e2.duration_in_min)
+    expect(page).to have_content(@e2.workout)
+    expect(page).to have_content(@e2.workout_date)    
+  end  
+end
